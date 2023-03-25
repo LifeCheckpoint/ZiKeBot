55769176e78b3b73bf9a583a78941da844a3570d
LDD <2435206827@qq.com>
2023-03-25 17:52:01 +0800

    tuning

--- modules/const_msg.py
+++ modules/const_msg.py
@@ -6,7 +6,8 @@ msg_dict = {
         "在！使用/help zike加载帮助",
         "不在！使用/help zike加载帮助",
         "嗯？",
-        "鳖载着理发店"
+        "鳖载着理发店",
+        "等一下就好！"
     ],
     "help.help": ["""梓刻Bot By. LDD
 提示：方括号为可选参数
@@ -35,11 +36,13 @@ zike 或 hi zike - Ping
     ],
     "sum.msg_limit": [
         "还没聊够三十条(o-ωｑ)).oO",
-        "至少要新聊30条才能调用oωo"
+        "至少要新聊30条才能调用oωo",
+        "不要着急，再聊几条再说＞﹏＜"
     ],
     "sum.reply": [
-        "最近至多%d条消息总结：\n（o(TヘTo)没钱交电费，珍惜调用次数）\n",
-        "最近%d条，群友聊了...\n（o(TヘTo)没钱交电费，珍惜调用次数）\n"
+        "最近至多%d条消息总结：\n（o(TヘTo)没钱交电费，珍惜调用次数）\n\n",
+        "最近%d条，群友聊了...\n（o(TヘTo)没钱交电费，珍惜调用次数）\n\n",
+        "我从最近%d条消息看出了群友风气（\n（o(TヘTo)没钱交电费，珍惜调用次数）\n\n"
     ],
     "sum.api_calling_failed": [
         "API调用失败了...使用/err查看原因",
@@ -47,11 +50,13 @@ zike 或 hi zike - Ping
     ],
     "sum.no_err": [
         "偷着乐吧，还没错误( •̀ ω •́ )",
-        "没有错误！（震声）"
+        "没有错误！（震声）",
+        "誒，没错啊？Σ(っ °Д °;)っ"
     ],
     "sum.api_key_missing": [
         "没找到API Key...",
-        "老是乱丢API Key，找不见了吧~"
+        "老是乱丢API Key，找不见了吧~",
+        "API Key找不到，不关我事哦"
     ],
     "sum.api_setting": [
         "API Key设置好了❤",
@@ -59,11 +64,13 @@ zike 或 hi zike - Ping
     ],
     "sum.api_setting_illegal": [
         "这不是API Key吧...？",
-        "API Key不长这样啊(。Д。)"
+        "API Key不长这样啊(。Д。)",
+        "这是哪门子API Key(。・・)ノ"
     ],
     "sum.api_setting_clear": [
         "洒扫庭除，清除OK！",
-        "删掉API Key咯！"
+        "删掉API Key咯！",
+        "抹去，是为了更好地归来~"
     ]
 }
 
