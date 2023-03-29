e131418c038aab04f8fdc172f25743a06f9f0203
LDDMiao <2435206827@qq.com>
2023-03-29 09:23:08 +0800

    add config

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pkgutil
+import yaml
+from pathlib import Path
 from creart import create
 from graia.ariadne.app import Ariadne
 from graia.ariadne.connection.config import (
@@ -10,6 +12,7 @@ from graia.ariadne.model import Group
 from graia.broadcast import Broadcast
 from graia.saya import Saya
 
+
 saya = create(Saya)
 bcc = create(Broadcast)
 app = Ariadne(
@@ -18,9 +21,14 @@ app = Ariadne(
         "LDDMiao",
     ),
 )
+config_path = Path(__file__, "config.yaml").resolve()
+with open(str(config_path), "r") as stream:
+    bot_config = yaml.safe_load(stream)
 
 with saya.module_context():
     for module in pkgutil.iter_modules(["modules"]):
+        if module.name in bot_config["unloaded_plugins"]:
+            continue
         saya.require(f"modules.{module.name}")
 
 app.launch_blocking()
